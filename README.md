7f1248d39a4fa605abc73174dc43a85c8c3d5c68
Greg Nau <gregnau@live.nl>
2016-04-21 13:13:58 +0200

    complete-rework

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # ATtiny Weather Station
-Small but accurate device to collect environmental temperature and relative humidity which is sent through RF 433/315mhz. It uses the protocol of [Alecto WS-1700](http://www.alecto.nl/weerstation-1700) weather stations, since that is recognized by a great domotica solution: [Pilight] (http://www.pilight.org).
-Compared to my other weather station running on Arduino Nano (328P) it is a power saving device, consumes only a few milliamps!
-Therefore it can run off a single 9v battery for almost a month. Everything is detailed below to build yourself one, but in case you have questions, i'm glad to answer! The project is yet not complete, there can be still issues and mistypes so watch out.
+Small and accurate device to collect environmental temperature and relative humidity which is sent through RF 433/315mhz. Transmission uses the protocol of [Alecto WS-1700](http://www.alecto.nl/weerstation-1700) weather stations, since that is recognized by a great domotica solution: [Pilight](http://www.pilight.org). Compared to [my other weather station](http://www.github.com/gregnau/arduino_weather_station) running on Arduino Nano (328P) it is a power saving device: consuming only a few microamps!
+Therefore it can run off a single 9v battery for months, maybe years. Everything is detailed below to build yourself one, but in case you have questions or issues, i'm glad to answer/help. The project is still not complete, watch out for any mistakes! 
 
 ## Parts-list
- - [**Atmel ATtiny85 microchip**](http://www.atmel.com/images/atmel-2586-avr-8-bit-microcontroller-attiny25-attiny45-attiny85_datasheet.pdf)
+ - [**Atmel ATtiny85 microchip**](http://www.atmel.com/images/atmel-2586-avr-8-bit-microcontroller-attiny25-attiny45-attiny85_datasheet.pdf) (tested to work with)
  - [**DHT22/AM2302 sensor**](https://www.sparkfun.com/datasheets/Sensors/Temperature/DHT22.pdf)
  - [**RF 433 transmitter module (3.3v)**](http://www.dx.com/nl/p/mini-3-3v-rf-transmitter-receiver-module-433mhz-wireless-link-kit-for-arduino-3-3v-mcu-414004#.VrDg8Hko-Uk)
- - [**LT1763-3.3v micropower reg.**](http://cds.linear.com/docs/en/datasheet/1763fh.pdf)
- - [**battery holder or adapter (9v)**](http://www.ebay.com/sch/i.html?_from=R40&_trksid=p2050601.m570.l1313.TR0.TRC0.H0.X9v+battery+arduino.TRS0&_nkw=9v+battery+arduino&_sacat=0)
+ - [**battery holder or adapter (4.5v)**]()
  - [**Tactile SPST switch**](http://www.ebay.com/sch/i.html?_from=R40&_trksid=p2050601.m570.l1313.TR10.TRC2.A0.H0.Xspst+switch+smd.TRS0&_nkw=spst+switch+smd&_sacat=0) (momentary pushbutton)
  - [**few different resistors**](http://www.ebay.com/sch/i.html?_from=R40&_trksid=p2050601.m570.l1313.TR12.TRC2.A0.H0.Xresistor+pack.TRS0&_nkw=resistor+pack&_sacat=0) (see the [Schematic](#schematic))
  - [**two capacitors**](http://www.ebay.com/sch/i.html?_from=R40&_trksid=p2050601.m570.l1313.TR12.TRC2.A0.H0.Xcapacitor+pack.TRS0&_nkw=capacitor+pack&_sacat=0) (see the [Schematic](#schematic))
@@ -30,22 +28,16 @@ Hopefully this clarifies the above picture!
 ![ATtiny Weather Station](/schematics.jpg?raw=true "Schematics")
 
 ## Configuration
-There are some options available on top of the code:
+There is a configuration block on top of the main code file.
+It's possible to set here the options, which is hardcoded or controlled by switches on Alecto Ws devices.
 
-	// Set pins where modules connected
-	#define TX_PIN 0  // Transmitter pin
-	#define DHT_PIN 1  // DHT Sensor pin
+	// CONFIG
+	#define INTERVAL 120  // frequency of sending the data in seconds.
+	#define CHAN 1  // set channel (0:1 or 1:2 or 2:3)
+	#define REP 7  // signal repeats (default:7)
+	#define ID 1318  // device id (1280-1535)
 
-	// Alecto WS1700 protocol settings
-	#define ID 1334  // Set an ID between 1280 and 1535
-	#define BAT 1  // Battery status to send in signal (0:Depleted, 1:Charged)
-	#define TXMOD 0  // Sending mode (0:Auto,1:Forced)
-	#define CHAN 0  // Set channel (0:1 or 1:2 or 2:3)
-	#define REP 6  // Signal repeats
-
-	// Serial terminal(you need USB TTL rx pin connected to PB3)
-	//#define SERTER    // Un-comment this line to enable serial debug
-Pretty much self-explanatory, though it is not necessary to change any of these for basic operation. More information on the `SERTER` is described in the [Troubleshooting](#troubleshooting) section below.
+Pretty much self-explanatory, though it is not necessary to change any of these for basic operation.
 
 ## Compile
 Lately i've switched to [Arduino IDE 1.6.5](http://arduino.cc) on every of my computer, so i would suggest to use the same version.
@@ -59,24 +51,9 @@ To compile there is [DHTlib v0.1.13 (by Rob Tillaart)](http://github.com/RobTill
 ![Build | Passing](https://img.shields.io/badge/build-passing-brightgreen.svg)
 
 ## Troubleshooting
-In case you did everything as described, but still experiencing issues then you can try debugging via serial interface.
-The code file begins with a commented line `SERTER` which you need to uncomment and upload to the ATtiny.
+Most common issue is with the timing, caused by the internal oscillator of the ATtinys. These devices stated to be calibrated, but it is advised to do it again yourself. This can be done easily with [TinyTuner](http://ernstc.dk/arduino/tinytuner.html) and a [USB-TTL cable](http://www.ebay.com/sch/i.html?_from=R40&_trksid=p2050601.m570.l1313.TR0.TRC0.H0.TRS0&_nkw=usb+ttl&_sacat=0). For more information please check out the links.
 
-	// Serial terminal(you need USB TTL rx pin connected to PB3)
-	//#define SERTER    // Un-comment this line to enable serial debug
-Then you can connect a USB TTL's rx pin on the tiny's PB3 pin. Though this way there is no sending from the terminal, but you can see some texts confirming status of the program. Baud rate must be set to 9600bps or if you prefer faster speeds than look for the function `serialStart();` and change it.
-
-Then you should see something like this printing in the terminal window of the:
-
-	ATtiny Weather Station v1.0
-	SERIAL TERMINAL - ENABLED
-	TX PIN - D3
-	DHT PIN - D2
-	
-	MESSAGE 0101111100111000000011001011001010111 SYNC
-			ID = 1334	CH = 1	TEMP = 20.1	  HUM = 56
-	SENT
-If it displays everything like above, then you should take a look on the wiring.
+Serial terminal have been removed, because it were causing more trouble than anything. Besides it was just garbage many time, printing unreadable characters. Instead there is the `ledBlink(n)` function, where "`n`" is times the led will blink.
 
 
 ## Reference
--- attiny_weather_station.ino
+++ attiny_weather_station.ino
@@ -1,212 +1,91 @@
-/* ATtiny Weather Station v1.0
+/* ATtiny Weather Station v3.2
  * with Alecto WS1700 protocol.
  * 
  * Tiny,low power device designed to collect and send
  * environmental temperature and relative humidity.
+ * Tested to work nice with ATtiny85, but should be ok
+ * on other tiny variants.
+ *
+ * http://github.com/gregnau/attiny_weather_station
  *
  * GregNau      2016
  */
 
-// Serial terminal(you need USB TTL rx pin connected to PB3)
-//#define SERTER    //Un-comment this line to enable serial debug
+// SETTINGS
+#define INTVAL 60  // frequency of sending the data in seconds.
+#define CHAN 1  // set channel bit (0=CH1,1=CH2,2=CH3, )
+#define REP 7  // signal repeats (default=7x)
+//#define ID 1318  // device id (1280-1535) [when disabled, random id on start]
 
-// Set pins where modules connected
-#define TX_PIN 1  // Transmitter pin
-#define DHT_PIN 0 // DHT Sensor pin
+// sleep function libraries
+#include <avr/sleep.h>
+#include <avr/power.h>
+#include <avr/wdt.h>
 
-// Alecto WS1700 protocol settings
-#define ID 1334  // Set an ID between 1280 and 1535
-#define BAT 1    // Battery status to send in signal (0:Depleted, 1:Charged)
-#define TXMOD 0  // Sending mode (0:Auto,1:Forced)
-#define CHAN 0   // Set channel (0:1 or 1:2 or 2:3)
-#define REP 6    // Signal repeats
-
-// Alecto WS1700 protocol timings (!do not change this,unless...!)
-#define LONG 3780    // LONG is approx 3780 uS
-#define SHORT 1890   // SHORT is approx 1890 uS
-#define SEP 540      // SEParator is approx 540 uS
-#define SYNC 9180    // SYNC signal length is 9180 uS
-
-#include <dht.h>  // initialize DHT Library
+// initialize DHT sensor library
+#include <dht.h>
 dht DHT;
 
-byte message[36]; // array to hold the whole message
+// timing values of the signal
+const unsigned short PULSE_LENGTH = 272;
+const unsigned short LONG = PULSE_LENGTH*14;
+const unsigned short SHORT = PULSE_LENGTH*7;
+const unsigned short SEP = PULSE_LENGTH*2;
+const unsigned short SYNC = PULSE_LENGTH*34;
+
+// variables of sleep code
+volatile bool watchdogActive = false;
+byte sleepCycles = 0;
+
+// buffer to store message ready-to-send by bits
+byte message[36];
+
+long scale_constant = 112640L; // default scale_constant (1.1*1024*100)
+
+// define watchdog timer interrupt action
+ISR(WDT_vect) {
+  watchdogActive = true;  // set flag
+}
 
 void setup() {
-  pinMode(4, OUTPUT);   // status led
-  pinMode(TX_PIN, OUTPUT);  // initialize TX_PIN for sending
+  //OSCCAL = 0x4E;  // internal oscillator calibration value (important! see manual...)
   
-  #if defined(SERTER)
-    serialStart();  // start serial and print some text
-  #endif
+  pinMode(2, OUTPUT);  // STATUS LED
+  pinMode(1, OUTPUT);  // RF TRANSMITTER
+  pinMode(0, INPUT);  // DHT SENSOR
+
+  calibration(); // calibrate battery readings if VCC1 present
+
+  initMessage();  // set constant message bits (id,ch)
+  updateMessage(1);  // refresh message data (bat,txmode,temp,hum)
+  sendMessage(REP);  // send the message on rf transmitter for 'REP' times
+
+  setup_watchdog();  // set 8s watchdog timer interrupt
 }
 
 void loop() {
-  // Fill the message array
-  convertId(ID);
-  message[12] = BAT;
-  message[13] = TXMOD;
-  message[14-15] = CHAN;
-  convertTemp(readTemp());
-  convertHum(readHum());
-  
-  #if defined(SERTER)
-    serialPrintMessage(); // print the message bits on terminal if enabled
-    parseMessage(); // print the data in decimal and detailed
-  #endif
-  
-  digitalWrite(4, HIGH);  // turn led on during send
-  
-  for (byte r=0; r<REP; r++) { sendMessage(); }  // repeat the signal for above defined times
+  if (watchdogActive) {  // if there was a watchdog wakeup
+    watchdogActive = false;  // reset the flag
+    sleepCycles += 1;
 
-  digitalWrite(4, LOW); // turn status led off when sent
-  
-  #if defined(SERTER)
-    Serial.println("SENT");
-    Serial.flush();
-  #endif
-  
-  delay(180000);  // delay resend with 180s = 3minutes
-  // this is not yet battery friendly, to be solved soon
-}
+    if (sleepCycles >= INTVAL/8) {
+      updateMessage(0);
+      sendMessage(REP);
 
-void serialStart() {
-    Serial.begin(9600);
-    Serial.println("ATtiny Weather Station v1.0");
-    Serial.print("TX PiN - D");Serial.println(TX_PIN);
-    Serial.print("DHT PiN - D");Serial.print(DHT_PIN);
-    Serial.println();
-    Serial.flush();
-}
-
-// convert the ID to bits and write to message array
-void convertId(int decValue) {
-    int binIndex = 0;
-    for (int i = 11; i >= 0; i--) {
-      int binValue = decValue >> i; 
-      if (binValue & 1) {
-        message[binIndex] = 1;
-        binIndex += 1;
-      }
-      else {
-        message[binIndex] = 0;
-        binIndex += 1;
-      }
-    }
-}
-
-// read DHT sensor temperature
-int readTemp() {
-  DHT.read22(DHT_PIN);
-  int temp = DHT.temperature * 10;
-  return temp;
-}
-// read DHT sensor humidity
-int readHum() {
-  DHT.read22(DHT_PIN);
-  int hum = DHT.humidity;
-  return hum;
-}
-
-// convert temp to bits and fill to message array
-void convertTemp(int decValue) {
-  int binIndex = 16;
-  for (int i = 11; i >= 0; i--) {
-    int binValue = decValue >> i; 
-    if (binValue & 1) {
-      message[binIndex] = 1;
-      binIndex += 1;
-    }
-    else {
-      message[binIndex] = 0;
-      binIndex += 1;
+      sleepCycles = 0;  // reset counter
     }
   }
+
+  sleep();  // go to sleep!
 }
 
-// convert hum to bits and fill to message array
-void convertHum(int decValue) {
-  int binIndex = 28;
-  for (int i = 7; i >= 0; i--) {
-    int binValue = decValue >> i; 
-    if (binValue & 1) {
-      message[binIndex] = 1;
-      binIndex += 1;
-    }
-    else {
-      message[binIndex] = 0;
-      binIndex += 1;
-    }
-  }
-}
 
-#if defined(SERTER)
-  // parse the message
-  void parseMessage() {
-    unsigned int id = 0;
-    for(byte i = 0; i < 12; i++) { id = (id << 1) + message[i]; }
-    Serial.print("ID = ");
-    Serial.print(id);
-    Serial.flush();
-  
-    unsigned short int ch = 0;
-    for(byte i = 15; i < 17; i++) { ch = (ch << 1) + message[i]; }
-    Serial.print(" CH = ");
-    Serial.print(ch);
-    Serial.flush();
-  
-    unsigned int temp = 0;
-    for(byte i = 17; i < 28; i++) { temp = (temp << 1) + message[i]; }
-    Serial.print(" TEMP = ");
-    Serial.print(temp);
-    Serial.flush();
-  
-    unsigned int hum = 0;
-    for(byte i = 28; i < 36; i++) { hum = (hum << 1) + message[i]; }
-    Serial.print(" HUM = ");
-    Serial.println(hum);
-    Serial.flush();
+// A very simple led blinking function for feedback
+void ledBlink(byte n) {
+  for (n; n>0; n--) {
+    digitalWrite(2, HIGH);
+    delay(80);
+    digitalWrite(2, LOW);
+    delay(80);
   }
-  
-  // print the whole message bit by bit
-  void serialPrintMessage() {
-    Serial.print("MESSAGE: ");
-    for (byte i=0; i<36; i++) {
-      Serial.print(message[i]);
-      Serial.flush();
-    }
-    Serial.println(" SYNC");
-    Serial.flush();
-  }
-#endif
-
-// replay the value b on tx-pin
-void sendBit(byte b) {
-  if (b == 0) {               // LOW
-    digitalWrite(TX_PIN, HIGH);
-    delayMicroseconds(SHORT);
-    digitalWrite(TX_PIN, LOW);
-  }
-  else if (b == 1) {          // HIGH
-    digitalWrite(TX_PIN, HIGH);
-    delayMicroseconds(LONG);
-    digitalWrite(TX_PIN, LOW);
-  }
-  delayMicroseconds(SEP);
-}
-
-// SYNC signal
-void sendSync() {
-  digitalWrite(TX_PIN, HIGH);
-  delayMicroseconds(SYNC);
-  digitalWrite(TX_PIN,LOW);
-  delayMicroseconds(SEP);
-}
-
-// send the 'message' by bits
-void sendMessage() {
-  for (byte i = 0; i < 36; i++) {
-    sendBit(message[i]);
-  }
-  sendSync();    // send the SYNC signal at the end
 }
